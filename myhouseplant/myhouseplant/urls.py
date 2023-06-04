2e1f7d80fef2efa181cb53198a411b99e61bbc15
kimjoemaina <joemainak@gmail.com>
2023-06-04 18:03:42 +0300

    update urls.py static

--- myhouseplant/myhouseplant/urls.py
+++ myhouseplant/myhouseplant/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler403 = 'journal.views.error_403'
 handler404 = 'journal.views.error_404'
